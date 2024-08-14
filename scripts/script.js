c8859b24a203e74c5ff8e9436306429638f78f13
ms-sophia <jardelezasophia@gmail.com>
2024-08-15 01:19:06 +0800

    feature:changes made

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,61 @@
-let arrowComp = []; 
+let icons = []; 
 let arrowUser = [];
 let keys= [];
 let cnt = 0;
 let minutes = 1;
 let seconds = 0;
-let countPoint = 0;
+let points = 0;
 let numOfArrows = 6;
 let arrowOriginalColor = "#c21717"
-
+let arrowCorrectColor = "#fea712"
 function createArrow()
 {
     console.log(`entered createArrow`);
     console.log(`number of Arrow:${numOfArrows}`);
     var number = 0;
-    cnt = 0;
-    while (cnt < numOfArrows)
+    var counter = 0;
+    while (counter < numOfArrows)
         {
         number = Math.floor(Math.random()*numOfArrows)+1;
         console.log(number)
         switch (number) {
             case 1:
-            arrowComp.push('right');
-            keys.push('ArrowRight');
+            // arrowComp.push('right');
+            // keys.push('ArrowRight');
             
-            // direction = 'right';
-            // eventKey = 'ArrowRight';
+            direction = 'right';
+            eventKey = 'ArrowRight';
             break;
             case 2:
-            arrowComp.push('down');
-            keys.push('ArrowDown');
-            // direction = 'down'
-            // eventKey = 'ArrowDown';
+            // arrowComp.push('down');
+            // keys.push('ArrowDown');
+            direction = 'down'
+            eventKey = 'ArrowDown';
             break;
             case 3:
-            arrowComp.push('left');
-            keys.push('ArrowLeft');
-            // direction = 'left'
-            // eventKey = 'ArrowLeft';
+            // arrowComp.push('left');
+            // keys.push('ArrowLeft');
+            direction = 'left'
+            eventKey = 'ArrowLeft';
             break;
             case 4:
-            arrowComp.push('up');
-            keys.push('ArrowUp');
-            // direction = 'up'
-            // eventKey = 'ArrowUp';
+            // arrowComp.push('up');
+            // keys.push('ArrowUp');
+            direction = 'up'
+            eventKey = 'ArrowUp';
             break;
             default:
-            arrowComp.push('right');
-            keys.push('ArrowRight');
-            // direction = 'right'
-            // eventKey = 'ArrowRight';
+            // arrowComp.push('right');
+            // keys.push('ArrowRight');
+            direction = 'right'
+            eventKey = 'ArrowRight';
             break
             
         }
-        cnt++;
+        counter++;
+        icons.push(direction);
+        keys.push(eventKey);
     }
-    
 }    
 
 function displayArrow()
@@ -62,9 +63,9 @@ function displayArrow()
     console.log(`entered displayArrow`);
     for (let i = 0; i < numOfArrows; i++)
         {
-        var icon_i = document.getElementById(`arrow_${i}`);
+        var icon_i = document.getElementById(`arrow_${String(i)}`);
         icon_i.classList.remove('fa-arrow-right', 'fa-arrow-left', 'fa-arrow-up', 'fa-arrow-down');
-        icon_i.classList.add('fa-arrow-'+arrowComp[i]);
+        icon_i.classList.add('fa-arrow-'+icons[i]);
     }
 }
 
@@ -114,20 +115,21 @@ function restartTimer()
     minutes = 1;
     seconds = 0;
     displayTimer.textContent = formatTime(minutes, seconds);
-    intialize();
+    initialize();
 }
 
-function intialize()
+function initialize()
 {
     console.log(`entered initialize...`);
     cnt = 0;
-    arrowComp = [];
+    icons = [];
     arrowUser = [];
     keys = [];
-    
     for (let i = 0; i < numOfArrows; i++){
-        document.getElementById( `arrow_${0}`).style.color = arrowOriginalColor;
+        document.getElementById( `arrow_${String(i)}`).style.color = arrowOriginalColor;
     }
+    createArrow();
+    displayArrow();
     
 }
 
@@ -144,48 +146,32 @@ async function userInput() {
 
 async function game() {
     console.log('Game is starting...');
-    
     var displayPoints = document.getElementById('counter');
+    cnt = 0;
+    createArrow();
+    displayArrow();
     
-    while (true) {
-        
-        // * create the arrows for user to follow, has also randomizer inside
-        arrowComp = createArrow();
-        
-        // // * display arrow 
+    while (true)
+        {
         displayArrow();
-        
-        
+        console.log(`counting:${cnt}...`);
         await userInput();
-        
-        if (keys[cnt] === arrowUser[cnt])
-            {
-            messageBoard.textContent = 'Correct Arrow';
-            countPoint++;
-            document.getElementById(`arrow_${String(cnt)}`).style.color = "yellow";
+        if (arrowUser[cnt] === keys[cnt]) {
+            console.log(`CORRECT`);
+            displayPoints.textContent = `${++points} points`;
+            document.getElementById(`arrow_${cnt}`).style.color = arrowCorrectColor;
             cnt++;
-            if (cnt === 4)
-                {
-                console.log(`this is entered cnt=4`);
+            if (cnt === 6)
+            {
+                initialize();
             }
-            
         }
         else
         {
-            messageBoard.textContent = 'Wrong Arrow';
-            
-            // * initialize arrays,points and count
-            countPoint = 0;    
-            intialize(); 
+            initialize();
         }
-        
-        console.log(`Count:${cnt}`);
-        displayPoints.textContent = countPoint + " points";
 
     }
-    
-    
 }
 
-createArrow();
-displayArrow();
+game();
